ea247a5179f89669f0e169130c086cc54dffc9e9
amanieltewolde <aman94@hotmail.it>
2026-08-11 06:41:14 +0200

    ex Array n°1

---
+++ js/app(array).js
@@ -0,0 +1,21 @@
+console.log('ciao array');
+
+// 1.
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+// array vuoto
+// const userOddNumber = [];
+// console.log(userOddNumber);
+
+
+
+// // creare ciclo richieste
+// for(i = 1; i <= 6; i++) {
+//     let userNumb = Number(prompt('Inserisci un numero'));
+
+//     if(userNumb % 2 !== 0) {
+//         userOddNumber.push(userNumb);
+//         console.log(userOddNumber);
+//     } 
+// }

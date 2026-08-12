14a1aae78c8bc800b2b4e62c85f444d77ec2c615
amanieltewolde <aman94@hotmail.it>
2026-08-13 00:00:32 +0200

    ex Array n°2

--- js/app(array).js
+++ js/app(array).js
@@ -19,3 +19,13 @@ console.log('ciao array');
 //         console.log(userOddNumber);
 //     } 
 // }
+
+// 2.
+// Dato l'array di numeri [10, 21, 6, 73, 9, 14, 44], crea un array con soli numeri pari
+// const numb = [10, 21, 6, 73, 9, 14, 44];
+// console.log(numb);
+
+// const evenNumb = numb.filter(n=> n % 2 === 0);
+
+// console.log(evenNumb);
+
